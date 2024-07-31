a905367c6a44c8a68d9736c62c2bc86fda77fc2a
hasan13222 <jamil8305@gmail.com>
2024-07-31 23:12:51 +0600

    backend functionality complete

--- src/app/modules/cart/cart.service.ts
+++ src/app/modules/cart/cart.service.ts
@@ -1,14 +1,19 @@
 import mongoose from 'mongoose';
 import { Product } from '../products/products.model';
 
+// fetching cart added products
 const getProductsByIdsFromDB = async (ids: string[]) => {
-    if (!ids || Object.keys(ids).length === 0){
-        return [];
-    }
+  if (!ids || Object.keys(ids).length === 0) {
+    return [];
+  }
+
+  // making objectid from _id string
   const objectIds = ids.map((id) => {
     const ObjectId = new mongoose.Types.ObjectId(id);
     return ObjectId;
   });
+
+  // fetching products by array of objectids
   const result = await Product.find({ _id: { $in: objectIds } });
   return result;
 };
